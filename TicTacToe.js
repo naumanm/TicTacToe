efabde263916a82ad60b4a6aa5f30c877784e470
michael nauman <bodhiandmike@gmail.com>
2015-01-16 15:42:08 -0800

    adding js file

---
+++ TicTacToe.js
@@ -0,0 +1,49 @@
+function startButtonReady () {
+
+}
+
+function clickSquare(squareValue) {
+
+	/* need to pass in state, X or O
+	onclick change state, 
+	make unselectable
+	*/
+
+}
+
+function startGame() {
+	/* button click triggers main loop
+
+	 var winner = false
+	 var whosTurn = "X";
+
+	 loop while !winner 
+		prompt whoseTurn to pick a point
+		square check 
+			is clickable, use mouseover
+		
+		clickSquare(whosTurn);
+
+		onclick change state, 
+			assign whoseTurn
+			make unselectable
+
+		checkIfWinner();
+
+		if whosTurn = "X" {
+			set whosTurn = "O";
+		} else {
+			set whosTurn = "X";
+		};
+
+	*/
+
+}
+
+function checkIfWinner () {
+	// will check to see if there is a winner by 
+	// looking at the values of each 
+}
+
+// Lets get this party started
+window.onload = startButtonReady;
