41234ccc698a8af234f1fe86bd11e1e2d9d105e7
michael nauman <bodhiandmike@gmail.com>
2015-01-18 13:36:46 -0800

    loops working, need to wait for user click

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,58 +3,67 @@
 window.onload = main;
 
 function main() {
-  // event listeners
+  // Start game listener
   document.getElementById("myStartButton").addEventListener("click", gameLoop);
 }
 
 function gameLoop() {
+  //---------------------------------------
+  // setup 
   var whosTurn = "X";
   var winner = false;
 
+  // get a list of all the game squares
+  var mySquares = document.getElementsByClassName("mySquare");
+
+  // Assign listener to the game squares
+  for(var j = 0; j < mySquares.length; j++) {
+    mySquares[j].addEventListener("click", nextTurn(whosTurn));	
+  }
+
+  //---------------------------------------
+  // main game loop
   for (var i = 1; i < 10; i++) {
-    // get all the squares 
-    var mySquares = document.getElementsByClassName("mySquare");
-    
-    console.log("number of squares");    
-    console.log(mySquares.length);
-
-    // adding event listeners to each square on gameboard
-    // TODO: need to only add selectable squares, do all for now
-    for(var j = 0; j < mySquares.length; j++) {
-      mySquares[j].addEventListener("click", nextTurn(whosTurn));	
+    // debugging
+    console.log("gameLoop: " + i);
+    console.log("whos turn " + whosTurn);
 
 
- //   var reset = document.getElementById("reset");
- //    reset.addEventListener("click", function (event) {
- //      // Notice how I can reuse the groceries variable from above.
- //      for (var i = 0; i < groceries.length; i++) {
- //        groceries[i].setAttribute("class", "");
- //      }
- //      document.querySelector("img").setAttribute("src", "images/panic.jpeg");
- //    });
-
-
-    }
+  //   var reset = document.getElementById("reset");
+  //    reset.addEventListener("click", function (event) {
+  //      // Notice how I can reuse the groceries variable from above.
+  //      for (var i = 0; i < groceries.length; i++) {
+  //        groceries[i].setAttribute("class", "");
+  //      }
+  //      document.querySelector("img").setAttribute("src", "images/panic.jpeg");
+  //    });
 
       // checks to see if the last click won the game
-	  //checkIfWinner();
+	checkIfWinner();
 
     if (whosTurn == "X") {
       whosTurn = "O"; 
     } else {
       whosTurn = "X";
     }
-   
-    console.log("gameLoop: " + i);
-    console.log("whos turn " + whosTurn);
+  }
+} 
 
-  } 
-}
 
 function nextTurn(whosTurn) {
 
-    console.log("should not be seeing this unless there has been a click");
     console.log("nextTurn: " + whosTurn);
+    var selected = false;
+
+
+    //var myID = event.target.getElementById.getAttribute("id");
+
+    //console.log(myID);
+    
+    /*  This is going to wait for a click
+    while (selected) {
+	}
+    */
 
 	/* need to pass in state, X or O
 	onclick change state, 
@@ -69,5 +78,8 @@ function nextTurn(whosTurn) {
 
 function checkIfWinner () {
 	// will check to see if there is a winner by 
+	//if (winner) {
+      console.log("The winner is?" );
+	//}
 	// looking at the values of each 
 }
