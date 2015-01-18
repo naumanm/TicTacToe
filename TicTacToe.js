923f3eb971ac0e31f8d4bf35fae1135a9ea88a97
michael nauman <bodhiandmike@gmail.com>
2015-01-18 12:59:50 -0800

    updating command loop, functions, and classes

--- TicTacToe.js
+++ TicTacToe.js
@@ -4,48 +4,67 @@ window.onload = main;
 
 function main() {
   // event listeners
-  document.getElementById("myStartButton").addEventListener("click", startGame);
+  document.getElementById("myStartButton").addEventListener("click", gameLoop);
 }
 
-function startGame() {
-  console.log("Start the Game!");
+function gameLoop() {
   var whosTurn = "X";
   var winner = false;
 
   for (var i = 1; i < 10; i++) {
-      // prompt user for input
-  	  // wait for user input...
-/*  	
+    // get all the squares 
+    var mySquares = document.getElementsByClassName("mySquare");
+    
+    console.log("number of squares");    
+    console.log(mySquares.length);
 
-		square check
-		is clickable, use mouseover
-		
-		clickSquare(whosTurn);
+    // adding event listeners to each square on gameboard
+    // TODO: need to only add selectable squares, do all for now
+    for(var j = 0; j < mySquares.length; j++) {
+      mySquares[j].addEventListener("click", nextTurn(whosTurn));	
 
-		onclick change state, 
-		assign whoseTurn
-		make unselectable
-       next turn
 
-*/
-	  checkIfWinner();
+ //   var reset = document.getElementById("reset");
+ //    reset.addEventListener("click", function (event) {
+ //      // Notice how I can reuse the groceries variable from above.
+ //      for (var i = 0; i < groceries.length; i++) {
+ //        groceries[i].setAttribute("class", "");
+ //      }
+ //      document.querySelector("img").setAttribute("src", "images/panic.jpeg");
+ //    });
 
-      if (whosTurn == "X") {
-        whosTurn = "O"; 
-      } else {
-        whosTurn = "X";
-      }
-      console.log(whosTurn);
+
+    }
+
+      // checks to see if the last click won the game
+	  //checkIfWinner();
+
+    if (whosTurn == "X") {
+      whosTurn = "O"; 
+    } else {
+      whosTurn = "X";
+    }
+   
+    console.log("gameLoop: " + i);
+    console.log("whos turn " + whosTurn);
 
   } 
 }
 
-function clickSquare(squareValue) {
+function nextTurn(whosTurn) {
+
+    console.log("should not be seeing this unless there has been a click");
+    console.log("nextTurn: " + whosTurn);
 
 	/* need to pass in state, X or O
 	onclick change state, 
 	make unselectable
 	*/
+//		square check
+//		is clickable, use mouseover
+
+
+
 }
 
 function checkIfWinner () {
