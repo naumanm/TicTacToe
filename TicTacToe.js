dcf7cdd1f8a91d10aabf1a56cb25a08d0ccb0e25
michael nauman <bodhiandmike@gmail.com>
2015-01-19 11:23:01 -0800

    getting ids from clicks and assigning to the dom, check winner is next

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,30 +1,36 @@
 // - mikeNauman --
 
-
 window.onload = main;
 
-var whosTurn = "X";
-
+  var whosTurn = "X";
 
 function main() {
+  // TODO: Should do a reset
+  // resetGame();
   // Start game listener
   console.log(whosTurn); 
   document.getElementById("myStartButton").addEventListener("click", startGame);
 }
 
 function startGame() {
+  
+  //document.getElementById("myStartButton").removeEventListener("click", startGame, false);
+  
+/*
   //---------------------------------------
   // get a list of all the game squares
-  var mySquares = document.getElementsByClassName("mySquare");
-  console.log(mySquares.length);
-  console.log(mySquares);
+  // var mySquares = document.getElementsByClassName("mySquare");
+  // console.log(mySquares.length);
+  // console.log(mySquares);
 
   // Assign listener to the game squares "mySquares[j]"" 
   // for(var j = 1; j < mySquares.length; j++) {
   //   mySquares[j].addEventListener("click", oneTurn);
   // }
+*/
 
-  // TODO: work around for the for loop above
+
+  // TODO: work around for the For loop above
   document.getElementById("p1").addEventListener("click", oneTurn);
   document.getElementById("p2").addEventListener("click", oneTurn);
   document.getElementById("p3").addEventListener("click", oneTurn);
@@ -40,31 +46,21 @@ function startGame() {
 //---------------------------------------
 // 
 function oneTurn () {
-  console.log("whosTurn :" + whosTurn);
-  // change image
+  var myID = this.id;  
   event.target.setAttribute("src", "./images/" + whosTurn + ".jpeg");
-  
 
+  event.target.setAttribute("class", whosTurn);
 
-  var test2 = event.target.querySelector("id");
-  console.log("I am test2 " + test2);
-
-  var myID = event.target.getAttribute("id");  
+  console.log("whosTurn :" + whosTurn);
   console.log("This should be the button id: " + myID);
-
-
-
+  
   // checks to see if the last click won the game
   checkIfWinner();
 
   if (whosTurn == "X") {
   	whosTurn = "O";
-  	console.log("whosTurn (O):" + whosTurn);
-    return "O"; 
   } else {
     whosTurn = "X";
-  	console.log("whosTurn (X):" + whosTurn);
-    return "X";
   } 
 }
 
